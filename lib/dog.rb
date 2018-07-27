42eb325974de33d513c8680bd7e2f827f41192f0
kate harris <kateharris03@gmail.com>
2018-07-27 15:55:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,9 @@
-# dog.rb
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+    puts "I am a dog! I am #{@name}! I am a #{@breed} dog!"
+  end
+end
+
+spot = Dog.new("Spot", "lab")
